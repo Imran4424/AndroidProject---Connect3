0aae81e9d57c275fb4bc121b616124dac5cc8e3b
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-27 22:12:33 +0600

    setting UI

--- app/src/main/java/com/luminousinc/connect3/MainActivity.java
+++ app/src/main/java/com/luminousinc/connect3/MainActivity.java
@@ -7,18 +7,26 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    // 0 - yellow, 1 - red
+    int activePlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activePlayer = 0;
     }
 
     public void dropIn(View tappedView) {
-        ImageView tappedCell = (ImageView) tappedView;
+        if(0 == activePlayer) {
+            ImageView tappedCell = (ImageView) tappedView;
 
-        tappedCell.setTranslationY(-2000);
-        tappedCell.setImageResource(R.drawable.yellow);
-        tappedCell.animate().translationYBy(2000).rotation(3600).setDuration(900);
+            tappedCell.setTranslationY(-2000);
+            tappedCell.setImageResource(R.drawable.yellow);
+            tappedCell.animate().translationYBy(2000).rotation(3600).setDuration(900);
+        }
+
+
     }
 }
