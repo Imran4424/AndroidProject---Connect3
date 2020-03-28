d0df57673561c33be7ed6ca1396e3f2e701f119b
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-28 23:25:28 +0600

    set congrats activity

--- app/src/main/java/com/luminousinc/connect3/congratsActivity.java
+++ app/src/main/java/com/luminousinc/connect3/congratsActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class congratsActivity extends AppCompatActivity {
+    TextView winnerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_congrats);
+        winnerText = (TextView) findViewById(R.id.textViewWinner);
     }
 
     public void playAgain(View v) {
