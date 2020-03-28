7a78b1bf1c74d2a54bfce7a6bd186373dae3720c
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-28 13:22:31 +0600

    set winning condition

--- app/src/main/java/com/luminousinc/connect3/MainActivity.java
+++ app/src/main/java/com/luminousinc/connect3/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // 0 - empty, 1 - red, 2 - yellow
@@ -59,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 winner = "Yellow";
             }
+
+            Toast.makeText(this, winner + " has Won", Toast.LENGTH_SHORT);
         }
     }
 
